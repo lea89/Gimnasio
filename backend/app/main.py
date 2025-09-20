d78a966c8b2de884496675c92242730b15faef4a
SAUBIETTE Leandro Alberto <saubietteleandro@gmail.com>
2025-09-20 19:35:26 -0300

    Modificaciòn de Main

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,5 +6,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Gimnasio MVP")
 
+
+@app.get("/health")
+def health_check():
+    return {"status": "Render backend is running!"}
+
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(alumnos.router, prefix="/alumnos", tags=["alumnos"])
